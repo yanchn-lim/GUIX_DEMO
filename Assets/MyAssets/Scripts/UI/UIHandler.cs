8a609cc56329733c50848cf84729857554290923
dingledangledongle <105861890+yanchn-lim@users.noreply.github.com>
2024-02-04 03:02:23 +0800

    tjsaipdksa

--- Assets/MyAssets/Scripts/UI/UIHandler.cs
+++ Assets/MyAssets/Scripts/UI/UIHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIHandler : MonoBehaviour
 {
@@ -15,5 +16,17 @@ public class UIHandler : MonoBehaviour
     {
         obj.SetActive(false);
         MouseHandler.LockMouse();
+    }
+
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("TownScene");
+        MouseHandler.LockMouse();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
